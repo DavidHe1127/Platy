2ba91c53c0bfa4761f50bd9feff9e59972ea010f
David He <jialhe85@gmail.com>
2020-02-06 23:47:10 +1100

    update sg

--- infra/vpc/alb-sg.tf
+++ infra/vpc/alb-sg.tf
@@ -11,18 +11,18 @@ resource "aws_security_group" "dockerzon-lb-sg" {
 
 resource "aws_security_group_rule" "allow-outbound-traffic-to-instance" {
   type                     = "egress"
-  from_port                = 80
-  to_port                  = 80
-  protocol                 = "tcp"
+  from_port                = 0
+  to_port                  = 0
+  protocol                 = "-1"
   source_security_group_id = aws_security_group.app-instance-sg.id
   security_group_id        = aws_security_group.dockerzon-lb-sg.id
 }
 
-resource "aws_security_group_rule" "allow-inbound-traffic-from-http" {
+resource "aws_security_group_rule" "allow-inbound-traffic" {
   type              = "ingress"
-  from_port         = 80
-  to_port           = 80
-  protocol          = "tcp"
+  from_port         = 0
+  to_port           = 0
+  protocol          = "-1"
   cidr_blocks       = ["0.0.0.0/0"]
   ipv6_cidr_blocks  = ["::/0"]
   security_group_id = aws_security_group.dockerzon-lb-sg.id
--- infra/vpc/ec2-sg.tf
+++ infra/vpc/ec2-sg.tf
@@ -10,11 +10,19 @@ resource "aws_security_group" "app-instance-sg" {
 
 resource "aws_security_group_rule" "allow-inbound-traffic-from-alb" {
   type                     = "ingress"
-  from_port                = 80
-  to_port                  = 80
-  protocol                 = "tcp"
+  from_port                = 0
+  to_port                  = 0
+  protocol                 = "-1"
   source_security_group_id = aws_security_group.dockerzon-lb-sg.id
-  ipv6_cidr_blocks         = ["::/0"]
   security_group_id        = aws_security_group.app-instance-sg.id
 }
 
+resource "aws_security_group_rule" "allow-outbound-traffic-off-instance" {
+  type              = "egress"
+  from_port         = 0
+  to_port           = 0
+  protocol          = "-1"
+  cidr_blocks       = ["0.0.0.0/0"]
+  ipv6_cidr_blocks  = ["::/0"]
+  security_group_id = aws_security_group.app-instance-sg.id
+}
