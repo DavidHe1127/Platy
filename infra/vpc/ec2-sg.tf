68b2b00a28ca08f366a7d487a2df9d1360869c1c
David He <jialhe85@gmail.com>
2020-02-11 22:26:15 +1100

    update vpc

--- infra/vpc/ec2-sg.tf
+++ infra/vpc/ec2-sg.tf
@@ -31,6 +31,7 @@ resource "aws_security_group_rule" "allow-ssh-from-anywhere" {
   from_port         = 22
   to_port           = 22
   protocol          = "tcp"
+  cidr_blocks       = ["0.0.0.0/0"]
   security_group_id = aws_security_group.app-instance-sg.id
 }
 
