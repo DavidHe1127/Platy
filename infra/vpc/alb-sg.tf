397f568d88e71dd80efca977eed59d3be83daa1e
David He <jialhe85@gmail.com>
2020-03-17 09:09:40 +1100

    alb sg update

--- infra/vpc/alb-sg.tf
+++ infra/vpc/alb-sg.tf
@@ -9,22 +9,30 @@ resource "aws_security_group" "dockerzon-lb-sg" {
 
 }
 
-resource "aws_security_group_rule" "allow-outbound-traffic-to-instance" {
-  type                     = "egress"
-  from_port                = 0
-  to_port                  = 0
-  protocol                 = "-1"
-  source_security_group_id = aws_security_group.app-instance-sg.id
-  security_group_id        = aws_security_group.dockerzon-lb-sg.id
+resource "aws_security_group_rule" "allow-inbound-traffic-from-https" {
+  type              = "ingress"
+  from_port         = 443
+  to_port           = 443
+  protocol          = "tcp"
+  cidr_blocks       = ["0.0.0.0/0"]
+  security_group_id = aws_security_group.dockerzon-lb-sg.id
 }
 
-resource "aws_security_group_rule" "allow-inbound-traffic" {
+resource "aws_security_group_rule" "allow-inbound-traffic-from-http" {
   type              = "ingress"
-  from_port         = 0
-  to_port           = 0
-  protocol          = "-1"
+  from_port         = 80
+  to_port           = 80
+  protocol          = "tcp"
   cidr_blocks       = ["0.0.0.0/0"]
   security_group_id = aws_security_group.dockerzon-lb-sg.id
 }
 
 
+resource "aws_security_group_rule" "allow-outbound-traffic-to-instance" {
+  type                     = "egress"
+  from_port                = 80
+  to_port                  = 80
+  protocol                 = "tcp"
+  source_security_group_id = aws_security_group.app-instance-sg.id
+  security_group_id        = aws_security_group.dockerzon-lb-sg.id
+}
