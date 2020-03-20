a3b1a2fce16f617c98032040cfe7c0c1627b3902
David He <jialhe85@gmail.com>
2020-03-20 15:41:15 +1100

    Add placement constraint example

--- infra/ecs/ec2/bootstrap/index.sh
+++ infra/ecs/ec2/bootstrap/index.sh
@@ -7,3 +7,4 @@ echo "ECS_CLUSTER=${cluster}" > /etc/ecs/ecs.config
 # use ECS_INSTANCE_ATTRIBUTES to set custom attributes which is used to
 # help determine where to place your task
 # only working on a unregistered instance
+# echo ECS_INSTANCE_ATTRIBUTES='{"location": "instanceTwo"}' >> /etc/ecs/ecs.config
