4f41bdfbb005dc3ba6461aa808c889a72fb87afb
David He <jialhe85@gmail.com>
2020-04-29 11:51:17 +1000

    wip4

--- ops-work/update-amis/drain-old-instances.sh
+++ ops-work/update-amis/drain-old-instances.sh
@@ -1,19 +1,27 @@
 #!/bin/bash
 
+OLD_AMI=ami-06862a6ef1260bb02
+NEW_AMI=ami-064db566f79006111
+
 # use ($string) to convert a string to array
-INSTANCES=($(aws ecs list-container-instances \
-  --cluster dockerzon \
-  --query 'containerInstanceArns' \
-  --output text \
-  --status ACTIVE))
+# the outermost () construct the result into an array
+INSTANCES=$(aws ecs list-attributes \
+    --target-type container-instance \
+    --attribute-name ecs.ami-id \
+    --cluster dockerzon \
+    --output json \
+    --query 'attributes' \
+    | jq -r -c --arg ami "$OLD_AMI" '[.[] | select(.value == $ami)]')
 
-echo $INSTANCES
+_jq() {
+ echo $1 | base64 --decode | jq -r $2
+}
 
-for instance in "${INSTANCES[@]}"
-do
-   echo $instance
+for instance in $(jq -r '.[] | @base64' <<< "$INSTANCES"); do
+   OLD_INSTANCE=$(_jq $instance '.targetId')
+   echo $OLD_INSTANCE
    aws ecs list-tasks \
     --cluster dockerzon \
-    --container-instance $instance \
+    --container-instance $OLD_INSTANCE \
     --desired-status RUNNING
 done
