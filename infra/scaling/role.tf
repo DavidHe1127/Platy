798691e3c7fd883c8b487e3960eef175d5cc061c
David He <jialhe85@gmail.com>
2020-05-06 08:35:46 +1000

    fix service-role creation

--- infra/scaling/role.tf
+++ infra/scaling/role.tf
@@ -1,15 +1,8 @@
-resource "aws_iam_role" "app-auto-scaling-service-role" {
-  name                  = "AWSServiceRoleForApplicationAutoScaling_ECSService"
-  assume_role_policy    = file("configs/assumer_role_policy.json")
-  force_detach_policies = true
-
-  tags = {
-    Purpose = "Application auto scaling service role"
-  }
+resource "aws_iam_service_linked_role" "ecs-app-auto-scaling-role" {
+  aws_service_name = "ecs.application-autoscaling.amazonaws.com"
 }
 
 resource "aws_iam_role_policy_attachment" "policy-attachment" {
-  role       = aws_iam_role.app-auto-scaling-service-role.name
+  role       = aws_iam_service_linked_role.ecs-app-auto-scaling-role.name
   policy_arn = "arn:aws:iam::aws:policy/aws-service-role/AWSApplicationAutoscalingECSServicePolicy"
 }
-
