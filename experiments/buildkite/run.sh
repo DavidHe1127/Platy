e82e625d407b6b5ee9f2b230c7d58eba0fff5e1d
David He <jialhe85@gmail.com>
2020-10-13 21:05:14 +1100

    test build

--- experiments/buildkite/run.sh
+++ experiments/buildkite/run.sh
@@ -2,5 +2,5 @@
 
 
 docker run --rm \
-  -v "/Users/davidhe/.ssh:/root/.ssh:ro" \
+  -v "/Users/davidhe/.ssh:/root/.ssh" \
   -e BUILDKITE_AGENT_TOKEN="2e7f35daff6531202185540f2f6d396486e1bcf34a4e6d429d" buildkite/agent
